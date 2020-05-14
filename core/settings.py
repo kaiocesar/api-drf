85bca11becb88b9312d6acbe7b52568d4cae1a0b
Kaio Cesar <tecnico.kaio@gmail.com>
2020-05-13 22:16:28 -0300

    create new app snippets

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
 
 MIDDLEWARE = [
